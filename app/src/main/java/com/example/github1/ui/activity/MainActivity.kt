85b9767fe52c5640f679fb51b2e9ee107ec8f031
Elinabai <elinabazarbaikyzy017@gmail.com>
2023-05-13 17:32:08 +0600

    добавила Fragment Conteiner View

--- app/src/main/java/com/example/github1/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/github1/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.github1
+package com.example.github1.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.github1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
